ef7534b0ab597853182cc9972d92d014c053f856
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-18 16:23:27 +0200

    complete task 2 with SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+//EventListeners
+galleryRef.addEventListener('click', onGalleryImgClick);
+
+function onGalleryImgClick(e) {
+    if (!e.target.classList.contains('gallery__image')) return;
+    e.preventDefault();
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
+
+const makeGalleryHtmlMarkup = (imgs) => {
+    return imgs.map(({preview, original, description}) => {
+        return `
+                  <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                  </a>
+               `
+    }).join("");
+}
+
+const insertMarkup = (ref, markupStr) => {
+    ref.innerHTML = markupStr;
+}
+
+insertMarkup(galleryRef, makeGalleryHtmlMarkup(galleryItems));
+
+// console.log(galleryItems);
